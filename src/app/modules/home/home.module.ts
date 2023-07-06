105379ff1e43253ef14651a9959f53faba813dc1
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-07-06 13:19:52 -0500

    tabla de persona

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { PeopleTableComponent } from 'src/app/components';
 
 
 
@@ -11,7 +12,8 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    PeopleTableComponent
   ]
 })
 export class HomeModule { }
