43df84019e245e9b252d042595425f3ecce7ae8d
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-07-06 14:20:13 -0500

    modal

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { PeopleTableComponent } from '../people-table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-toolbar',
-  imports: [MatToolbarModule, MatIconModule],
+  imports: [CommonModule,MatToolbarModule, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(PeopleTableComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 }
